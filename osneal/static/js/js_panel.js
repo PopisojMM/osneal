e03a64f0c4857330aee0d768397ca60385ce816c
PopisojMM <66846834+PopisojMM@users.noreply.github.com>
2023-03-17 12:04:02 -0300

    js_panel
    Agregado JS del panel con todos los scripts (Vacunas, buscadores, panel lateral)

---
+++ osneal/static/js/js_panel.js
@@ -0,0 +1,54 @@
+let btn = document.querySelector("#btn");
+let sidebar = document.querySelector(".sidebar");
+let searchBtn = document.querySelector(".bx-search");
+const dropdownButton = document.querySelector(".dropdown-button");
+const dropdownContent = document.querySelector(".dropdown-content");
+
+
+btn.onclick = function(){
+    sidebar.classList.toggle("active");
+}
+searchBtn.onclick = function(){
+    sidebar.classList.toggle("active");
+}
+
+
+
+//////////////////////////////////////////////////////// QUERY BUSCADOR DNI
+// Obtener el formulario y el campo de entrada de texto
+const form = document.querySelector('#search-form');
+const input = document.querySelector('#search-dni');
+
+// Manejar el evento de envío del formulario
+form.addEventListener('submit', (event) => {
+  // Prevenir la acción predeterminada del envío del formulario
+  event.preventDefault();
+  
+  // Obtener el valor del campo de entrada de texto
+  const searchQuery = input.value.trim();
+  
+  // Realizar la búsqueda si el campo de entrada de texto no está vacío
+  if (searchQuery !== '') {
+    // Aquí puedes agregar tu código para realizar la búsqueda con el DNI ingresado
+    console.log(`Buscando DNI ${searchQuery}...`);
+  }
+});
+
+///////////// VACUNAS 
+const animalTableBody = document.getElementById('animalTableBody');
+const agregarAnimalForm = document.querySelector('form');
+
+agregarAnimalForm.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const nombre = document.getElementById('nombre').value;
+  const especie = document.getElementById('especie').value;
+  const fechaAplicacion = document.getElementById('fechaAplicacion').value;
+  const proximaVacunacion = document.getElementById('proximaVacunacion').value;
+
+  const newRow = document.createElement('tr');
+  newRow.innerHTML = `
+    <td>${nombre}</td>
+    <td>${especie}</td>
+    <td>${fechaAplicacion}</td>
+    <td>${proximaVacunacion}</td>
